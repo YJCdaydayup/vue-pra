8a042e6ec9bc2ad780dad4118f7bccf8055c6e9b
Your Name <you@example.com>
2019-01-13 22:57:34 +0800

    开始goods模块

--- 12.vue移动端完整项目/sell/src/components/goods/goods.vue
+++ 12.vue移动端完整项目/sell/src/components/goods/goods.vue
@@ -1,22 +1,50 @@
 <template>
     <div class="goods">
-        I am goods
+      <div class="menu-wrapper"></div>
+      <div class="foods-wrapper"></div>
     </div>
 </template>
-<style>
-  .goods {
-    width: 100%;
-    height: 500px;
-    background-color: red;
-  }
+<style lang="stylus" rel="stylesheet/stylus">
+  .goods
+    display: flex
+    position: absolute
+    top: 174px
+    bottom: 46px
+    width: 100%
+    overflow: hidden
+    .menu-wrapper
+      flex: 0 0 80px
+      width: 80px
+      background-color: #f3f5f7
+    .foods-wrapper
+      flex: 1
+      background-color: #fff
+
+
 </style>
 
 <script>
+
+    const ERR_OK =0;
+
     export default {
+        props: {
+          seller: {
+            type: Object
+          }
+        },
         data() {
             return {
-                msg: "hello vue"
+              goods: []
             }
+        },
+        created() {
+          this.$http.get('/api/goods').then((res)=>{
+            if (res.data.errno == ERR_OK) {
+              this.goods = res.data.data;
+              console.log(this.goods)
+            }
+          })
         },
         components: {}
     }
