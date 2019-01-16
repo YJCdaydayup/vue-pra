3f6d6d197b7624f37ccc4e47ab5c5aed7ccfc824
Your Name <you@example.com>
2019-01-17 07:39:54 +0800

    通过计算属性实现购物车

--- 12.vue移动端完整项目/sell/src/components/shopcart/shopcart.vue
+++ 12.vue移动端完整项目/sell/src/components/shopcart/shopcart.vue
@@ -6,11 +6,16 @@
             <div class="logo">
               <i class="icon-shopping_cart"></i>
             </div>
+            <div class="number">{{totalCount}}</div>
           </div>
-          <div class="price">0元</div>
+          <div class="price">{{totalPrice}}元</div>
           <div class="desc">另需配送费¥{{deliveryPrice}}</div>
         </div>
-        <div class="content-right"></div>
+        <div class="content-right">
+          <div class="pay">
+            ¥{{minPrice}}元起送
+          </div>
+        </div>
       </div>
     </div>
 </template>
@@ -25,6 +30,7 @@
     .content
       display: flex
       background-color: #141d27
+      color: rgba(255,255,255,0.4)
       .content-left
         flex: 1
         .logo-wrapper
@@ -61,20 +67,22 @@
           border-right: 1px solid rgba(255,255,255,0.1)
           font-size: 16px
           font-weight: 700
-          color: rgba(255,255,255,0.4)
         .desc
           display: inline-block
           vertical-align: top
           margin: 12px 0 0 5px
           line-height: 24px
           font-size: 10px
-          color: rgba(255,255,255,0.4)
       .content-right
         flex: 0 0 100px
         width: 100px
-        height: 48px
-        background-color: rgba(255,255,255,0.1)
-
+        .pay
+          height: 48px
+          line-height: 48px
+          text-align: center
+          font-size: 12px
+          font-weight: 700
+          background-color: #2b333b
 
 
 </style>
@@ -82,6 +90,17 @@
 <script>
     export default {
         props: {
+          selectFoods: {
+            type: Array,
+            default() {
+              return [
+//                {
+//                  price: 10,
+//                  count: 1
+//                }
+              ]
+            }
+          },
           deliveryPrice: {
             type: Number,
             default: 0
@@ -90,6 +109,23 @@
             type: Number,
             default: 0
           }
+        },
+        computed: {
+          totalPrice() {
+            let total = 0;
+            this.selectFoods.forEach((food)=>{
+              total += food.price * food.count
+            })
+            return total;
+          },
+          totalCount() {
+            let count = 0
+            this.selectFoods.forEach((food)=>{
+              count += food.count
+            })
+            return count;
+          }
+
         },
         data() {
             return {
