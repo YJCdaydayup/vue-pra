fcd7dbac67cb26d69b1d5dac41c6f6c44355f519
Your Name <you@example.com>
2019-01-22 23:02:14 +0800

    提交

--- 12.vue移动端完整项目/sell/src/components/food/food.vue
+++ 12.vue移动端完整项目/sell/src/components/food/food.vue
@@ -1,34 +1,49 @@
 <template>
   <transition name="move">
-    <div class="food" ref="food" v-show="showFlag">
-      <div class="food-content">
-        <div class="image-header">
-          <img :src="food.image">
-          <div class="back" @click="back">
-            <i class="icon-arrow_lift"></i>
+    <div class="food" v-show="showFlag" ref="foodWrapper">
+      <div class="foodWrapper">
+        <div class="food-content">
+          <div class="image-header">
+            <img :src="food.image">
+            <div class="back" @click="back">
+              <i class="icon-arrow_lift"></i>
+            </div>
           </div>
         </div>
-      </div>
-      <div class="content">
-        <h1 class="title">{{food.name}}</h1>
-        <div class="detail">
-          <span class="sell-count">月售{{food.sellCount}}</span>
-          <span class="rating">好评率{{food.rating}}</span>
+        <div class="content">
+          <h1 class="title">{{food.name}}</h1>
+          <div class="detail">
+            <span class="sell-count">月售{{food.sellCount}}</span>
+            <span class="rating">好评率{{food.rating}}</span>
+          </div>
+          <div class="price">
+            <span class="now">¥{{food.price}}</span>
+            <span class="old" v-show="food.oldPrice">¥{{food.oldPrice}}</span>
+          </div>
+          <div class="cartcontrol-wrapper">
+            <cartcontrol :food="food"></cartcontrol>
+          </div>
+          <transition name="fade">
+            <div @click="addFirst($event)" class="buy" v-show="!food.count || food.count===0">加入购物车</div>
+          </transition>
         </div>
-        <div class="price">
-          <span class="now">¥{{food.price}}</span>
-          <span class="old" v-show="food.oldPrice">¥{{food.oldPrice}}</span>
+        <split v-show="food.info"></split>
+        <div class="info" v-show="food.info">
+          <h1 class="title">商品信息</h1>
+          <p class="text">{{food.info}}</p>
+        </div>
+        <split></split>
+        <div class="rating">
+          <h1 class="title">商品评价</h1>
+          <ratingselect></ratingselect>
         </div>
       </div>
-      <div class="cartcontrol-wrapper">
-        <cartcontrol :food="food"></cartcontrol>
-      </div>
-      <div class="buy"></div>
     </div>
   </transition>
 </template>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  * { touch-action: pan-y; }
   .food
     position: fixed
     left: 0
@@ -61,10 +76,9 @@
           padding: 10px
           color: #fff
           font-size: 20px
-
-
     .content
       padding: 18px
+      position: relative
       .title
         line-height: 14px
         margin-bottom: 8px
@@ -92,12 +106,51 @@
           text-decoration: line-through
           font-size: 10px
           color: gray
+      .cartcontrol-wrapper
+        position: absolute
+        right: 12px
+        bottom: 12px
+      .buy
+        position: absolute
+        right: 18px
+        bottom: 18px
+        z-index: 10px
+        height: 24px
+        line-height: 24px
+        padding: 0 12px
+        box-sizing: border-box
+        font-size: 10px
+        border-radius: 12px
+        color: #fff
+        background-color: rgb(0,160,220)
+        transition: all 0.5s
+        opacity: 1
+        &.fade-enter,&.fade-leave-to
+          opacity: 0
+
+
+
+    .info
+      padding: 18px
+      .title
+        line-height: 14px
+        margin-bottom: 6px
+        font-size: 14px
+        color: rgb(7,17,27)
+      .text
+        line-height: 24px
+        padding: 0 8px
+        font-size: 12px
+        color: rgb(77,85,93)
+
 </style>
 
 <script>
 
     import BScroll from 'better-scroll'
     import cartcontrol from './../cartcontrol/cartcontrol.vue'
+    import split from './../split/split.vue'
+    import ratingselect from './../ratingselect/ratingselect.vue'
 
     export default {
         props: {
@@ -110,26 +163,35 @@
               showFlag: false
             }
         },
+        mounted() {
+          this.scroll = new BScroll(this.$refs.foodWrapper, {
+            click: true
+          })
+        },
         methods: {
           show() {
             this.showFlag = true;
             // 在哪里出现在哪里调用，记得要用refresh
             this.$nextTick(()=>{
-              if (!this.scroll) {
-                this.scroll = new BScroll(this.$refs.food,{
-                  click: true
-                })
-              }else {
-                this.scroll.refresh()
+              if (this.scroll) {
+                this.scroll.refresh();
               }
             })
           },
           back() {
             this.showFlag = false;
+          },
+          addFirst(event) {
+            if (!event._constructed) {
+              return;
+            }
+            this.$set(this.food,'count',1);
           }
         },
         components: {
-          cartcontrol
+          cartcontrol,
+          split,
+          ratingselect
         }
     }
 </script>
