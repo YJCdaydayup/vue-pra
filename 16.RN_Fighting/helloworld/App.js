a38b4e4183886b67e7519854976d46cd597bc32d
Your Name <you@example.com>
2019-01-29 08:08:23 +0800

    环境配置

--- 16.RN_Fighting/helloworld/App.js
+++ 16.RN_Fighting/helloworld/App.js
@@ -22,7 +22,9 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.welcome}>
+          haha hehe heihei
+        </Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
