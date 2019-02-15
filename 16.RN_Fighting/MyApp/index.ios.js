65097a1faa246bb667cf5c5fc44b7712bd154701
Your Name <you@example.com>
2019-02-15 08:01:44 +0800

    点击事件

--- 16.RN_Fighting/MyApp/index.ios.js
+++ 16.RN_Fighting/MyApp/index.ios.js
@@ -9,20 +9,61 @@ import {
     AppRegistry,
     StyleSheet,
     View,
-    Image,
     Text,
-    TextInput
+    TouchableOpacity,
+    AlertIOS
 } from 'react-native';
 
-var LoginView = require('./loginView')
-
 export default class Test3 extends Component {
+
+    // 状态机
+    state = {
+        title: '默认值'
+    }
+
     render() {
         return (
-           <LoginView/>
+            <View style={styles.container}>
+                <TouchableOpacity
+                    activeOpacity={0.1}
+                    onPress={()=> {
+                        this.click('点击')
+                    }}
+                    onPressIn={()=> {
+                        this.click('按下')
+                    }}
+                    onPressOut={()=> {
+                        this.click('抬起')
+                    }}
+                    onLongPress={()=> {
+                        this.click('长按')
+                    }}
+                >
+                    <View>
+                        <Text>{this.state.title}</Text>
+                    </View>
+                </TouchableOpacity>
+            </View>
         )
     }
+
+    click(event) {
+        // console.log(e)
+        // AlertIOS.alert('来了')
+        this.setState({
+            title: event
+        })
+    }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#f3f5f7'
+    }
+});
 
 
 // 输出到iOS App里面
