1d451f3aa852ed8e8a8a92de53c27b572db5ab6b
yangli <yangli@example.com>
2019-02-25 17:17:24 +0800

    修改sum

---
+++ 16.RN_Fighting/RN_Official/CCZQ/ios/CCZQViewManager.h
@@ -0,0 +1,17 @@
+//
+//  CCZQViewManager.h
+//  CCZQ
+//
+//  Created by cczq3 on 2019/2/25.
+//  Copyright © 2019年 Facebook. All rights reserved.
+//
+
+#import "RCTViewManager.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CCZQViewManager : RCTViewManager
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ 16.RN_Fighting/RN_Official/CCZQ/ios/CCZQViewManager.m
@@ -0,0 +1,13 @@
+//
+//  CCZQViewManager.m
+//  CCZQ
+//
+//  Created by cczq3 on 2019/2/25.
+//  Copyright © 2019年 Facebook. All rights reserved.
+//
+
+#import "CCZQViewManager.h"
+
+@implementation CCZQViewManager
+
+@end
