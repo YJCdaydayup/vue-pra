c02f1777d1ca4cfb1ec48c65e06c68293ad042bb
yangli <yangli@example.com>
2019-08-08 17:18:32 +0800

    补充提交nodejs笔记

--- 05.nodejs基础语法/nodejs_sys_learn/src/05.debug调试.js
+++ 05.nodejs基础语法/nodejs_sys_learn/src/05.debug调试.js
@@ -1,12 +1,28 @@
 'use strict'
 
 var arr = [];
-for (let i = 0; i < 10; i++) {
+for (var i = 0; i < 10; i++) {
+  var x = i;
   arr[i] = function () {
-    return i;
+    return x;
   }
 }
 
 console.log(arr[8]())
+
+var a = 123
+if (true) {
+  var a = 234
+}
+console.log(a);
+
+var b = 'abc'
+function test() {
+  var b = 'cde'
+}
+
+test();
+
+console.log(b);
 
 
