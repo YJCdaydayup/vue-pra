85cf2f47cccda70cbc2541151884a5aaa1f288c8
Your Name <you@example.com>
2019-08-21 23:01:23 +0800

    补充

--- 05.nodejs基础语法/nodejs_sys_learn/src/07.http模块/server.js
+++ 05.nodejs基础语法/nodejs_sys_learn/src/07.http模块/server.js
@@ -12,22 +12,36 @@ const server = http.createServer();
 // response就是http.serverResponse对象 就是一个写文件流
 // request就是http.IncommingMessgae对象
 server.on('request', function (request, response) {
+    // 1./favicon.ico会多出一个请求来请求小图标，所以请求是两个,postman请求就没有，只有浏览器请求才有
+    // console.log(request.headers)
+    console.log(request.httpVersion);
+    console.log(request.method);
+    console.log(request.url)
+
+    // 2.什么参数都没有默认路径就是/,真正的路由就在这里这样写
+    if (request.url === '/login' && request.method === 'GET') {
+        response.end('<div>news</div>')
+    }else {
+        response.end('<div>login</div>')
+    }
+
+
     // 写报文头
     // 第一个状态码
     // 第二个是写入的内容的格式类型，浏览器解析不了就会下载下来,如svg图片，png图片
     // 报文头必须在前面写，一旦有报文扔出去再扔头，报文乱掉了，浏览器解析不了了
-    response.writeHead(200,{
-        'Content-Type': 'text/html;charset=utf-8',
-    })
-
-    // 向响应报文的报文体写内容
-    response.write('hello');
-    response.write('<h1>123</h1>')
-    // 这也说明了报文是一块一块的传递到客户端的
-    // 结束,end()必须放在最后面，代表报文已经扔回去了
-    // end('world')也可以传递参数
-    // end就只能写一次
-    response.end();
+    // response.writeHead(200,{
+    //     'Content-Type': 'text/html;charset=utf-8',
+    // })
+    //
+    // // 向响应报文的报文体写内容
+    // response.write('hello');
+    // response.write('<h1>123</h1>')
+    // // 这也说明了报文是一块一块的传递到客户端的
+    // // 结束,end()必须放在最后面，代表报文已经扔回去了
+    // // end('world')也可以传递参数
+    // // end就只能写一次
+    // response.end();
 });
 
 // node默认式3000端口，java默认8080
