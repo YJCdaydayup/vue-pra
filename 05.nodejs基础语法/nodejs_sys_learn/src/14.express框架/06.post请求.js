961f628e517fa00a1c277c8179473c15de0e0137
yangli <yangli@example.com>
2019-09-05 17:06:39 +0800

    补充

--- 05.nodejs基础语法/nodejs_sys_learn/src/14.express框架/06.post请求.js
+++ 05.nodejs基础语法/nodejs_sys_learn/src/14.express框架/06.post请求.js
@@ -5,18 +5,31 @@ const bodyParser = require('body-parser');
 
 let app = express();
 
-// app.use(bodyParser.json());
+app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({
     extended: false
 }))
 
-app.post('/',function (req, res) {
+app.post('/', function (req, res) {
     console.log(req.body);
-    res.setHeader('Access-Control-Allow-Origin','*')
-    res.setHeader('Access-Control-Allow-Methods','GET, OPTIONS')
-    res.setHeader('Access-Control-Allow-Headers','Origin, Accept, Content-Type')
-    res.send(JSON.stringify(req.body));
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS')
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, Accept, Content-Type')
+    // res.send(JSON.stringify(req.body));
+    res.json(req.body)
+})
+
+app.post('/post', (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    console.log(req.body)
+    res.json({
+        status: 200,
+        data: {
+            name: 'Sara',
+            age: 18
+        }
+    })
 })
 
 app.listen(3000);
