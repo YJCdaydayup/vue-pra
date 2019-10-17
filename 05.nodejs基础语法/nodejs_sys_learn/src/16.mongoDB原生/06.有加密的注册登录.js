6e53055e41d05037aad4830eabf140c66a043b7c
yangli <yangli@example.com>
2019-10-17 16:48:50 +0800

    补充

--- 05.nodejs基础语法/nodejs_sys_learn/src/16.mongoDB原生/06.有加密的注册登录.js
+++ 05.nodejs基础语法/nodejs_sys_learn/src/16.mongoDB原生/06.有加密的注册登录.js
@@ -7,17 +7,22 @@ const express = require('express');
 const form = require('formidable');
 const db = require('./model/db');
 const md5 = require('./model/md5');
+const session = require('express-session');
 
 let app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.static('./public'));
-
-app.get('/register', (req, res)=> {
+app.use(session({
+    secret: 'key',
+    resave: false,
+    saveUninitialized: true
+}))
+app.get('/register', (req, res) => {
     res.render('register');
 });
 
-app.post('/doregister', (req, res)=> {
+app.post('/doregister', (req, res) => {
     let formidable = new form.IncomingForm();
     formidable.parse(req, function (err, fileds, files) {
         // 同步的啊
@@ -25,7 +30,7 @@ app.post('/doregister', (req, res)=> {
         db.insertOne('users', {
             username: fileds.dengluming,
             password: mima
-        }, (err, result)=> {
+        }, (err, result) => {
             res.json({
                 status: 0,
                 result
@@ -34,44 +39,56 @@ app.post('/doregister', (req, res)=> {
     })
 });
 
-app.get('/login', (req, res)=> {
+app.get('/login', (req, res) => {
     res.render('nlogin')
 });
 
-app.post('/dologin', (req, res)=> {
+app.post('/dologin', (req, res) => {
     let formidable = new form.IncomingForm();
     formidable.parse(req, function (err, fileds, files) {
         // 同步的啊
         let mima = md5(md5(fileds.mima).substr(4, 7) + md5(fileds.mima));
         db.find('users', {
             username: fileds.dengluming,
-        }, (err, result)=> {
-           if (err) {
-               res.json({
-                   status: 1,
-                   msg: '系统错误'
-               });
-           }else {
-               if (result.length === 0) {
-                   res.json({
-                       status: 2,
-                       msg: '用户名错误'
-                   })
-               }
-               if (result[0].password == mima) {
-                   res.json({
-                       status: 0,
-                       msg: '登录成功'
-                   })
-               }else {
-                   res.json({
-                       status: 3,
-                       msg: '密码错误'
-                   })
-               }
-           }
+        }, (err, result) => {
+            if (err) {
+                res.json({
+                    status: 1,
+                    msg: '系统错误'
+                });
+            } else {
+                if (result.length === 0) {
+                    res.json({
+                        status: 2,
+                        msg: '用户名错误'
+                    })
+                }
+                if (result[0].password == mima) {
+                    req.session.login = 1;
+                    req.session.username = fileds.dengluming;
+                    req.session.password = mima;
+                    res.json({
+                        status: 0,
+                        msg: '登录成功'
+                    })
+                } else {
+                    res.json({
+                        status: 3,
+                        msg: '密码错误'
+                    })
+                }
+            }
         })
     })
+});
+
+app.get('/', (req, res) => {
+   let username = req.session.username;
+   if (req.session.login === 1) {
+       res.send('用户名是: ' + username);
+   }else {
+       res.send('请先登录');
+   }
 });
 
 app.listen(3000);
--- 05.nodejs基础语法/nodejs_sys_learn/src/16.mongoDB原生/07.图片处理gm.js
+++ 05.nodejs基础语法/nodejs_sys_learn/src/16.mongoDB原生/07.图片处理gm.js
@@ -6,15 +6,20 @@
 const fs = require('fs');
 const gm = require('gm');
 
-// gm('./public/imgs/err.jpg').resize(50,50,'!').write('./public/imgs/des.jpg',function (err) {
-//     console.log(err)
-//    if (!err) {
-//        console.log('done');
-//    }
-// });
+gm('./public/imgs/err.jpg').resize(50,50,'!').write('./public/imgs/des.jpg',function (err) {
+    console.log(err)
+   if (!err) {
+       console.log('done');
+   }
+});
 
-gm('./public/imgs/err.jpg')
-    .identify(function (err, data) {
-        console.log(err)
-        if (!err) console.log(data)
-    });
+gm('./public/imgs/err1.jpg')
+    .flip()
+    .magnify()
+    .rotate('green', 45)
+    .blur(7, 3)
+    .crop(300, 300, 150, 130)
+    .edge(3)
+    .write('./public/imgs/crazy.jpg', function (err) {
+        if (!err) console.log('crazytown has arrived');
+    })
