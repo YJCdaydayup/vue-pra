8b68d7015956646266a7127ae50bb2868128f102
Your Name <you@example.com>
2019-12-18 08:06:54 +0800

    几个面试要点

---
+++ 31.M_S_T_SUM/07.函数节流/01.jieliu.js
@@ -0,0 +1,35 @@
+function throttle(fn, delay) {
+    var lastTime = 0;
+    return function () {
+        var nowTime = Date.now();
+        if (nowTime - lastTime > delay) {
+            fn()
+            lastTime = nowTime;
+        }
+    }
+}
+
+function again(fn, delay) {
+    var timer;
+    return function () {
+        if (timer) {
+            return;
+        }
+        timer = setTimeout(function () {
+            fn()
+            clearTimeout(timer);
+        }, delay);
+    }
+}
+
+function debounce(fn, delay) {
+    var timer = null;
+    return function () {
+        clearTimeout(timer);
+        timer = setTimeout(function () {
+            fn.apply(this);
+        },delay)
+    }
+}
+
+
