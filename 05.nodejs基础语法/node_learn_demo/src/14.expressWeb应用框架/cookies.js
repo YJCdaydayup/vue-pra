58db71d01a9dc2c4ad374913c3e1dfcf465b8bab
yangli <yangli@example.com>
2020-01-08 17:36:29 +0800

    补充

--- 05.nodejs基础语法/node_learn_demo/src/14.expressWeb应用框架/cookies.js
+++ 05.nodejs基础语法/node_learn_demo/src/14.expressWeb应用框架/cookies.js
@@ -6,10 +6,16 @@ var app = express();
 app.use(cookieparser());
 
 app.get('/', function (req, res) {
-    console.log(req.cookies)
+    res.cookie('name','Sara');
+    res.send('设置了cookie')
 })
 
-var server = app.listen(8081, '172.23.1.128',function () {
+app.get('/abc',(req, res)=>{
+    console.log(req.cookies);
+    res.send(req.cookies.name);
+})
+
+var server = app.listen(8081,function () {
     console.log(server.address().address);
     console.log(server.address())
 })
