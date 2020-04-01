b466801520e799fcb602c2f75b864851bbe97507
Your Name <you@example.com>
2020-04-01 22:03:29 +0800

    完成初始socketio连接

--- 35.koa框架实战之预告片网站/07.koa/index.js
+++ 35.koa框架实战之预告片网站/07.koa/index.js
@@ -5,10 +5,32 @@ const views = require('koa-views')
 const session = require('koa-session')
 const path = require('path')
 const koaBody = require('koa-body')
+const serve = require('koa-static')
+const socketio = require('socket.io')
 
 const router = require('./router/index')
 
+global.sessionStorage = {}
+
 let app = new Koa()
+
+const server = require('http').Server(app.callback())
+let io = socketio(server)
+
+io.on('connection', (socket) => {
+    console.log('连接成功')
+    socket.on('connect-id', (id) => {
+        console.log('收到客户端发来的id：', id)
+        global.sessionStorage[id].sessionId = socket.id
+        // 广播发送在线人数信息
+        io.sockets.emit("online-info", global.sessionStorage);
+        // socket.broadcast.emit("online-info",global.sessionStorage);
+    })
+});
+
+app.on('error', async (err, ctx)=>{
+    console.log(err)
+})
 
 app.keys = ['some secret hurr'];
 
@@ -19,6 +41,16 @@ const CONFIG = {
     rolling: false
 }
 
+app.use(async (ctx, next)=>{
+    try {
+        await next()
+    }catch(err) {
+        ctx.render('err')
+    }
+})
+
+app.use(serve(path.resolve('./public')));
+
 app.use(session(CONFIG, app));
 
 app.use(koaBody())
@@ -27,4 +59,8 @@ app.use(views(path.resolve('./views'), {extension: 'ejs'}))
 
 app.use(router.routes()).use(router.allowedMethods())
 
-app.listen(3000)
+app.use(async (ctx) => {
+    await ctx.render('err')
+})
+
+server.listen(3000)
--- 35.koa框架实战之预告片网站/07.koa/router/index.js
+++ 35.koa框架实战之预告片网站/07.koa/router/index.js
@@ -1,7 +1,22 @@
 const Router = require('koa-router')
+const cryptoJS = require('crypto-js')
 
 let router = new Router({
-    prefix: '/chat'
+    prefix: ''
+})
+
+router.get('/', async (ctx, next)=>{
+    let id = ctx.query.id
+    if (!global.sessionStorage[id]) {
+        global.sessionStorage[id] = {}
+    }
+    global.sessionStorage[id].username = ctx.session.username
+    await ctx.render('index', {
+        username: ctx.session.username,
+        online: Object.keys(global.sessionStorage).length,
+        id,
+        onliner: global.sessionStorage
+    })
 })
 
 router.get('/login', async (ctx, next) => {
@@ -9,8 +24,15 @@ router.get('/login', async (ctx, next) => {
 })
 
 router.post('/doLogin', async (ctx, next) => {
-    console.log(ctx.request.body)
-    ctx.body = '1234'
+    let username = ctx.request.body.username
+    let id = Math.floor(Math.random() * 10000000).toString(16)
+    id = cryptoJS.MD5(id).toString()
+    ctx.session.username = username
+    ctx.session.id = id
+    ctx.body = {
+        code: 0,
+        id
+    }
 })
 
 module.exports = router
